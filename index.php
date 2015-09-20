216f84ebf20ebf47758a02c3761ea314a4364740
Hannah Riggs <hkriggs128@gmail.com>
2015-09-20 17:48:24 -0400

    Links updated for project 2.

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ CSCI E-15: Dynamic Web Applications
 					</div> 
 					<div class="col-lg-7 col-md-7 col-sm-7">
 						<p class="p-title title-1">Project 2 Title</p>
-						<a href="#" target="_blank" class="btn btn-primary">Live Site</a>
-						<a href="#" target="_blank" class="btn btn-primary">GitHub</a>
+						<a href="http://p2.hriggs.me" target="_blank" class="btn btn-primary">Live Site</a>
+						<a href="https://github.com/hriggs/dwa-project2" target="_blank" class="btn btn-primary">GitHub</a>
 						<p class="p-title">Project 3 Title</p>
 						<a href="#" target="_blank" class="btn btn-primary">Live Site</a>
 						<a href="#" target="_blank" class="btn btn-primary">GitHub</a>
